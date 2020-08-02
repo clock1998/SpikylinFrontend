b5cfa4b9abdcd497d6eb7acb5ae78dfb52efb65e
Linyi Jiang <jianglinyi97@gmail.com>
2020-08-02 18:22:13 -0400

    LAPI-17
    Gallery - adds upload function

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-import { first } from 'rxjs/operators';
 
+import { environment } from '../../../../../environments/environment';
 import { User } from '../../../../shared/models/user';
 import { UserService  } from '../../../../core/services/user.service';
 import { AuthenticationService  } from '../../../../core/services/authentication.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { GalleryService } from 'src/app/core/services/gallery.service';
 
 
-@Component({ templateUrl: 'dashboard.component.html' })
+@Component({
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: [ './dashboard.component.scss' ]
+})
 export class DashboardComponent {
     loading = false;
     users: User[];
-    user:User = {email:"", id:0, username:""};
+    user: User = {email:"", id:0, username:""};
+    form: FormGroup;
+    
+    response: any;
+    imageUrl: string;
 
-    constructor(private userService: UserService, private authService: AuthenticationService ) { }
+    constructor(private galleryService: GalleryService, 
+        private formBuilder: FormBuilder, 
+        private userService: UserService, 
+        private authService: AuthenticationService ) { }
 
     ngOnInit() {
         this.loading = true;
         this.getCurrentUser();
+        this.form = this.formBuilder.group({
+			image:['']
+		})
     }
 
     getCurrentUser(): void{
@@ -26,4 +42,30 @@ export class DashboardComponent {
             email:(data as any).email
         });
     }
+
+    onChange(event) {
+		if (event.target.files.length > 0) {
+		  const file = event.target.files[0];
+		  this.form.get('image').setValue(file);
+		}
+	}
+
+	onSubmit() {
+        const formData = new FormData();
+
+        formData.append('file', this.form.get('image').value);
+        formData.append('name', this.form.get('image').value.name);
+
+		this.galleryService.uploadImage(formData).subscribe(
+		  (res) => {
+			this.response = res;
+			this.imageUrl = `${res.file}/`;
+			console.log(res);
+			console.log(this.imageUrl);
+		  },
+		  (err) => {  
+			console.log(err);
+		  }
+		);
+	  }
 }
--- src/app/modules/gallery/pages/gallery/gallery.component.ts
+++ src/app/modules/gallery/pages/gallery/gallery.component.ts
@@ -21,19 +21,14 @@ import { trigger, state, style, animate, transition, query, group, animateChild 
 	]
 })
 export class GalleryComponent implements OnInit {
-	DJANGO_SERVER = 'http://127.0.0.1:8000';
 	images: Image[];
 	isLightBoxShown: boolean = false;
-	form: FormGroup;
-	response;
 	imageUrl: string = '';
 
-	constructor(private formBuilder: FormBuilder, private galleryService: GalleryService) {}
+	constructor( private galleryService: GalleryService) {}
 	ngOnInit(): void {
 		this.getImages();
-		this.form = this.formBuilder.group({
-			image:['']
-		})
+		
 	}
 
 	getImages(): void {
@@ -48,28 +43,4 @@ export class GalleryComponent implements OnInit {
 	hideLightBox(): void {
 		this.isLightBoxShown = false;
 	}
-
-	onChange(event) {
-		if (event.target.files.length > 0) {
-		  const file = event.target.files[0];
-		  this.form.get('image').setValue(file);
-		}
-	}
-
-	onSubmit() {
-		const formData = new FormData();
-		formData.append('file', this.form.get('image').value);
-	
-		this.galleryService.uploadImage(formData).subscribe(
-		  (res) => {
-			this.response = res;
-			this.imageUrl = `${this.DJANGO_SERVER}${res.file}/`;
-			console.log(res);
-			console.log(this.imageUrl);
-		  },
-		  (err) => {  
-			console.log(err);
-		  }
-		);
-	  }
 }
