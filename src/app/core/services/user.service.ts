414b4c8508beeef4cb5a81eb7b7592da7dd251ba
Linyi Jiang <jianglinyi97@gmail.com>
2020-09-10 22:38:38 -0400

    clear build warning

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { User } from '../../shared/models/user';
-import { map } from 'rxjs/internal/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
