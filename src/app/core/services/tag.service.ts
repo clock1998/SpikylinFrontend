485fb6e2aef33fe587746ae96a96a802649e8cf4
Linyi Jiang <jianglinyi97@gmail.com>
2021-01-30 12:59:25 -0500

    Update packages

--- src/app/core/services/tag.service.ts
+++ src/app/core/services/tag.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
 import { Tag } from 'src/app/shared/models/tag';
 import { BaseService } from './base.service';
 
